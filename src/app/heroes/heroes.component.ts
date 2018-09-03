eaeb3f43d6dc617ac28e0a3f0694e649094ff734
victorloo <ila9970@gmail.com>
2018-09-04 03:28:49 +0800

    Inject the HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
   }
